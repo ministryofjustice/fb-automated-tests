8d87442cb1935b51c2981fa33b718a7a0674fed8
Umar Hansa <umar.hansa@gmail.com>
2019-06-04 09:48:48 +0100

    Initial commit

---
+++ test.js
@@ -0,0 +1,42 @@
+import test from 'ava'
+import puppeteer from 'puppeteer'
+
+const headless = true
+
+async function withPage (t, run) {
+  const browser = await puppeteer.launch({headless})
+  const page = await browser.newPage()
+
+  page.getText = async (selector) => {
+    const result = await page.evaluate((_selector) => {
+      return window.document.querySelector(_selector).innerText;
+    }, selector);
+
+    return result;
+  }
+
+  try {
+    await run(t, page)
+  } finally {
+    await page.close()
+    await browser.close()
+  }
+}
+
+function sleep(ms = 1000) {
+	return new Promise(resolve => {
+		setTimeout(resolve, ms)
+	})
+}
+
+test('happy path in the form', withPage, async (t, page) => {
+  await page.goto('https://example.com')
+  const currentURL = await page.url()
+
+  console.log('Current URL: ', {currentURL});
+
+  t.is(currentURL, 'http://nope')
+
+  await sleep(2000)
+})
+
