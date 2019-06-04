64403b839c500f2cb86feafb60df85a6f271f752
Umar Hansa <umar.hansa@gmail.com>
2019-06-04 11:59:07 +0100

    Builds

--- test.js
+++ test.js
@@ -12,10 +12,10 @@ async function withPage (t, run) {
 
   page.getText = async (selector) => {
     const result = await page.evaluate((_selector) => {
-      return window.document.querySelector(_selector).innerText;
-    }, selector);
+      return window.document.querySelector(_selector).innerText
+    }, selector)
 
-    return result;
+    return result
   }
 
   try {
@@ -26,17 +26,9 @@ async function withPage (t, run) {
   }
 }
 
-function sleep(ms = 1000) {
-	return new Promise(resolve => {
-		setTimeout(resolve, ms)
-	})
-}
-
-test('gov uk', withPage, async (t, page) => {
+test.only('gov uk', withPage, async (t, page) => {
   await page.goto('http://gov.uk')
   const currentURL = await page.url()
-
-  console.log('Current URL: ', currentURL);
 
   t.is(currentURL, 'https://www.gov.uk/')
 })
@@ -45,16 +37,12 @@ test('hmcts', withPage, async (t, page) => {
   await page.goto('https://gov.uk/government/organisations/hm-courts-and-tribunals-service')
   const currentURL = await page.url()
 
-  console.log('Current URL: ', currentURL);
-
   t.is(currentURL, 'https://www.gov.uk/government/organisations/hm-courts-and-tribunals-service')
 })
 
 test('met office', withPage, async (t, page) => {
   await page.goto('https://metoffice.gov.uk')
   const currentURL = await page.url()
-
-  console.log('Current URL: ', currentURL);
 
   t.is(currentURL, 'https://www.metoffice.gov.uk/')
 })
@@ -63,8 +51,6 @@ test('tfl', withPage, async (t, page) => {
   await page.goto('https://www.tfl.gov.uk')
   const currentURL = await page.url()
 
-  console.log('Current URL: ', currentURL);
-
   t.is(currentURL, 'https://tfl.gov.uk/')
 })
 
@@ -72,16 +58,12 @@ test('civil service', withPage, async (t, page) => {
   await page.goto('http://civilservice.blog.gov.uk')
   const currentURL = await page.url()
 
-  console.log('Current URL: ', currentURL);
-
   t.is(currentURL, 'https://civilservice.blog.gov.uk/')
 })
 
 test('moj blog', withPage, async (t, page) => {
   await page.goto('http://mojdigital.blog.gov.uk')
   const currentURL = await page.url()
-
-  console.log('Current URL: ', currentURL);
 
   t.is(currentURL, 'https://mojdigital.blog.gov.uk/')
 })
